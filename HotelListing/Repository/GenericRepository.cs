31688140a0da21ff39c2246bef260a06ec49408a
Rabia İrem Uçar <ucanirem@outlook.com>
2022-04-26 20:51:22 +0300

    GenericRepository added

---
+++ HotelListing/Repository/GenericRepository.cs
@@ -0,0 +1,71 @@
+﻿using HotelListing.Data;
+using HotelListing.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace HotelListing.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly DataBaseContext context;
+        private readonly DbSet<T> db;
+        public GenericRepository(DataBaseContext dataBaseContext)
+        {
+            context = dataBaseContext;
+            db = context.Set<T>();
+        }
+        public async Task Delete(int id)
+        {
+            var entity = await db.FindAsync(id);
+            db.Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            db.RemoveRange(entities);
+        }
+
+        public async Task<T> Get(Expression<Func<T, bool>> expression, List<string> includes = null)
+        {
+            var query = db;
+
+            if (includes != null)
+                foreach (var include in includes)
+                    query.Include(include);
+
+            return await query.AsNoTracking().FirstOrDefaultAsync(expression);
+        }
+
+        public async Task<IList<T>> GetAll(Expression<Func<T, bool>> expression = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, List<string> includes = null)
+        {
+            IQueryable<T> query = db;
+            if (expression != null)
+                query.Where(expression);
+
+            if (includes != null)
+                foreach (var include in includes)
+                    query.Include(include);
+
+            if (orderBy != null)
+                query = orderBy(query);
+
+            return await query.AsNoTracking().ToListAsync();
+        }
+
+        public async Task Insert(T entity)
+        {
+            await db.AddAsync(entity);
+        }
+
+        public async Task InsertRange(IEnumerable<T> entities)
+        {
+            await db.AddRangeAsync(entities);
+        }
+
+        public void Update(T entity)
+        {
+            db.Attach(entity);
+            context.Entry<T>(entity).State = EntityState.Modified;
+        }
+    }
+}
